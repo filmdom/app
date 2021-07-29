0e9e2fd7217cde92525c2dab27bba590cc5c3258
dutra <dutra.makmur@gmail.com>
2021-07-29 22:46:37 +0700

    firts commit

--- 3. Berlangganan/app.js
+++ 3. Berlangganan/app.js
@@ -77,6 +77,25 @@ if(window.location.pathname != '/home.html'){
 
 }
 
+const phoneNumber = getPhoneNumberFromUserInput();
+const appVerifier = window.recaptchaVerifier;
+firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+    .then((confirmationResult) => {
+      // SMS sent. Prompt user to type the code from the message, then sign the
+      // user in with confirmationResult.confirm(code).
+      window.confirmationResult = confirmationResult;
+      // ...
+    }).catch((error) => {
+      // Error; SMS not sent
+      // ...
+    });
+
+    grecaptcha.reset(window.recaptchaWidgetId);
+
+// Or, if you haven't stored the widget ID:
+window.recaptchaVerifier.render().then(function(widgetId) {
+  grecaptcha.reset(widgetId);
+}
 
 // show error message function 
 function showErrorMessage(erro_message){
